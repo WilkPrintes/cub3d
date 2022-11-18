02b58deb122d26a4479d9e0fc13b2838cb34b9eb
WilkPrintes <wilkp90@gmail.com>
2022-11-18 10:59:11 -0300

    first commit

---
+++ Makefile
@@ -0,0 +1,51 @@
+NAME		:= cub3d
+
+CC			:= clang
+
+CCFLAGS		:= -Wall -Wextra -Werror
+
+HEADER_DIR	:= ./header/
+
+INCLUDE		:=	$(foreach directory, $(HEADER_DIR), -I $(directory))
+
+SRC_DIR		:= ./src/
+
+SRC_FILE	:=	cub3d.c
+
+SRC			:=	$(foreach file, $(SRC_FILE), $(SRC_DIR)$(file))
+
+LIBFT		:=	libraries/libft/libft.a
+
+LIBFT_PATH	:=	libraries/libft
+
+LIBFT_FLAGS	:=	-L $(LIBFT_PATH) -lft
+
+all: $(NAME)
+
+$(NAME): $(SRC) $(LIBFT)
+	@$(CC) -g $(CCFLAGS) $(SRC) -o $@ $(INCLUDE) $(LIBFT_FLAGS) 
+	@echo "\033[0;32mCub3d created ヽ(^o^)ノ \033[0m"
+
+$(LIBFT):
+	@make -s -C $(LIBFT_PATH)
+
+fclean: clean
+	@make fclean -s -C $(LIBFT_PATH)
+	@rm -rf $(NAME)
+	@echo "\033[0;31mCub3d deleted (⌣́ _⌣̀ ) \033[0m"
+
+clean:
+	@make clean -s -C $(LIBFT_PATH)
+
+re: fclean all
+
+$(OBJ_DIR):
+	@mkdir -p $@
+
+norma:
+	@norminette ./
+
+leaks:
+	@valgrind -q --tool=helgrind ./philo 2 200 100 100
+
+.PHONY: all fclean re
---
+++ libraries/libft/ft_strtrim.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wprintes < wprintes@student.42sp.org.br    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/23 15:33:09 by wprintes          #+#    #+#             */
+/*   Updated: 2022/02/11 02:41:24 by wprintes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = 0;
+	while (ft_strchr(set, s1[start]) != NULL)
+		start++;
+	while (ft_strchr(set, s1[ft_strlen((char *) s1) - end - 1]) != NULL)
+		end++;
+	return (ft_substr(s1, start, ft_strlen((char *) s1) - end - start));
+}
