47e213dead105a26c74a44275d8c1ff11265a272
B-Mugnol <bruno_mugnol@hotmail.com>
2023-03-11 21:26:12 -0300

    Update content to include all headers

--- header/master.h
+++ header/master.h
@@ -6,13 +6,20 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 15:10:09 by bmugnol-          #+#    #+#             */
-/*   Updated: 2023/03/11 15:11:30 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2023/03/11 20:07:18 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MASTER_H
 # define MASTER_H
 
+# include "macro.h"
+# include "define.h"
+# include "error.h"
+# include "verifier.h"
+# include "mlx_data.h"
+# include "mlx_hook.h"
+# include "render.h"
 # include "cub3d.h"
 
 #endif
