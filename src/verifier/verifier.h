696a0aa53be5d607294c7b96b5109c997ebe3d79
B-Mugnol <bruno_mugnol@hotmail.com>
2023-04-08 23:02:57 -0300

    Add file_access_verifier function

--- src/verifier/verifier.h
+++ src/verifier/verifier.h
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 18:05:44 by bmugnol-          #+#    #+#             */
-/*   Updated: 2023/03/11 19:05:22 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2023/04/07 14:42:29 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 # define VERIFIER_H
 
 # include <stdlib.h>
+# include <fcntl.h>
 # include "define.h"
 
 int	fd_verifier(int fd);
+int	file_access_verifier(char *filename);
 int	data_verifier(t_mlx_data *data);
 int	param_verifier(const int argc, const char *map_file);
 
