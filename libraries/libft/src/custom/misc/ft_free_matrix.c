f176e218f0c0147b49a998ac8d17647303fb47c0
B-Mugnol <bruno_mugnol@hotmail.com>
2023-05-04 16:09:01 -0300

    Add verification for NULL param

--- libraries/libft/src/custom/misc/ft_free_matrix.c
+++ libraries/libft/src/custom/misc/ft_free_matrix.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 18:02:33 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/03/14 17:39:02 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2023/05/04 16:06:02 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_free_matrix(void ***p, size_t line_count)
 {
 	size_t	i;
 
+	if (!*p)
+		return ;
 	i = 0;
 	while (i < line_count && *(*p + i) != NULL)
 	{
